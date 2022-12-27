c35730d7b8cc7ca39cbaf3b3320132fb90eba653
sanjeevbadwal3 <70515439+sanjeevbadwal3@users.noreply.github.com>
2022-12-27 17:57:21 +0530

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,7 @@
+# Original pool
+USER_POOL_ID='us-east-1_zlMIFVNk5'
+CLIENT_ID='4umpij11sai4f9k397qiek82h9'
+
+# New pool
+MIGRATED_USER_POOL_ID='us-east-1_q1HRpp4t2'
+MIGRATED_CLIENT_ID='8ccgad02omsihpu0pda5ks7b8'
